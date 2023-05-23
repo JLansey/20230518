329dddbe08638d6767d8f24cc7706aea862bc500
Jonathan Lansey <lansey+github@gmail.com>
2023-05-23 16:16:22 -0400

    .

--- Switch.h
+++ Switch.h
@@ -9,7 +9,7 @@
 ******************************************************************************************/
 
 //timing defines
-#define TIME_SWITCH_PRESS_DET	30	//Time in mS to detect Switch pressed
+#define TIME_SWITCH_PRESS_DET	15	//Time in mS to detect Switch pressed
 #define TIME_SWITCH_RELEASE_DET 15	//Time in mS to detect Switch released
 #define SWITCH_HORN_DEBOUNCE_INITIAL 29 // Starting the debounce counter up closer
 	
