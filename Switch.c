c1a709b9615f1ac30d444a732e986658023f5b9f
Jonathan Lansey <lansey+github@gmail.com>
2023-05-23 16:08:14 -0400

    changes to make start time faster

--- Switch.c
+++ Switch.c
@@ -34,7 +34,7 @@ void SwitchInit(void)
 
 	//Initialize variables used for Horn Switch
 	SwitchOldTick = 0;
-	SwitchHornDebounce = 0;
+	SwitchHornDebounce = SWITCH_HORN_DEBOUNCE_INITIAL; // 29
 	SwitchHornStatus = 0;
 }
 
